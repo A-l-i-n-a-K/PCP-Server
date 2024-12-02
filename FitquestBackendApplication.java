f288535ffaba14763d7dc2f553820d35e0d71b1d
A-l-i-n-a-K <145050880+A-l-i-n-a-K@users.noreply.github.com>
2024-12-02 23:43:03 +0300

    Add files via upload

---
+++ FitquestBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fitquest.fitquest_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitquestBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitquestBackendApplication.class, args);
+	}
+
+}
