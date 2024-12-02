d48f341249cd4046418686b263b01ab6ae4cbbcf
A-l-i-n-a-K <145050880+A-l-i-n-a-K@users.noreply.github.com>
2024-12-02 23:46:47 +0300

    Delete repository directory

--- repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.fitquest.fitquest_backend.repository;
-
-import com.fitquest.fitquest_backend.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
